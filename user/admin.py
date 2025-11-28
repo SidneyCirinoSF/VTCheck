ea41e4394a242e14258b3fdf5281ee364f155d20
SidneyCirinoSF <sidneycirinosf@gmail.com>
2025-11-28 11:17:37 -0300

    commit inicial com modulo de criação e autenticação de usuário pronto

---
+++ user/admin.py
@@ -0,0 +1,37 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.forms import TextInput, Textarea
+from .models import Usuario
+
+
+@admin.register(Usuario)
+class UsuarioAdmin(UserAdmin):
+    model = Usuario
+
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active', 'is_superuser')
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Informações pessoais', {'fields': ('first_name', 'last_name')}),
+        ('Permissões', {
+            'fields': (
+                'is_active',
+                'is_staff',
+                'is_superuser',
+                'groups',
+                'user_permissions'
+            )
+        }),
+        ('Datas importantes', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
